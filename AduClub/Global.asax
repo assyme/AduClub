6b7d9c11ce6c74bc2c19f29abdd6f481147e7a77
Asad Palekar <asad.palekar@gmail.com>
2014-05-19 10:27:43 +0400

    1st Default commit

---
+++ AduClub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AduClub.MvcApplication" Language="C#" %>
---
+++ AduClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AduClub.Startup))]
+namespace AduClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
