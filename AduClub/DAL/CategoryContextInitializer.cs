20a30b5e08e12ac2b8c3e77a7b6571e8f222d9b4
Asad Palekar <asad.palekar@gmail.com>
2014-05-20 17:48:19 +0400

    Implementing collections for clubs.

--- AduClub/DAL/CategoryContextInitializer.cs
+++ AduClub/DAL/CategoryContextInitializer.cs
@@ -4,21 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using AduClub.DAL;
+using AduClub.Models;
 
-namespace AduClub.Models
+namespace AduClub.DAL
 {
     public class ClubCentralContextInitializer : DropCreateDatabaseAlways<ClubCentralDBContext>
     {
         protected override void Seed(ClubCentralDBContext context)
         {
-            var categories = new List<Category>(){
-                new Category() {Name = "Science"},
-                new Category() {Name = "Art"},
-                new Category() {Name = "Commerce"}
+            var categories = new List<AduClub.Models.Category>(){
+                new AduClub.Models.Category() {Name = "Science"},
+                new AduClub.Models.Category() {Name = "Art"},
+                new AduClub.Models.Category() {Name = "Commerce"}
             };
 
             categories.ForEach(c => context.Categories.Add(c));
-            //context.Clubs.Add(new Club() { Name = "NodsJs", Description = "Club for Node JS Freaks." });
+            context.SaveChanges();
+
+            var clubs = new List<Club>();
+            var clubNode = new Club() { Name = "NodesJs", Description = "Club for Node JS Freaks.", CreatedOn = DateTime.Now };
+            clubNode.Categories = context.Categories.ToList();
+            
+            context.Clubs.Add(clubNode);
 
             context.SaveChanges();
         }
--- AduClub/Models/Club.cs
+++ AduClub/Models/Club.cs
@@ -11,7 +11,8 @@ namespace AduClub.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
         
-        //public virtual ICollection<Category> Categories { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
     } 
 }
